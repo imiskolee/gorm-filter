64407404f165f5eb5e424bd892332660d5b643b5
magicII <83399130@qq.com>
2022-12-08 07:33:10 +0000

    同步v1支持闭包拓展

--- filter_test.go
+++ filter_test.go
@@ -7,12 +7,25 @@ import (
 	"testing"
 )
 
+
 type Table struct {
 	ID int
 	A  int
 	B  int
 	C  int
 	D  int
+	E  int
+}
+
+type TableB struct {
+	ID int
+	A  int
+	B  int
+	C  int
+}
+
+func (t *TableB) TableName() string {
+	return "table_b"
 }
 
 func (t *Table) TableName() string {
@@ -35,33 +48,57 @@ func TestFilter(t *testing.T) {
 		"filter[a][_eq]=a&filter[b][_eq]=b&filter[c][_gt]=1": "SELECT * FROM `table` WHERE `a` = \"a\" AND `b` = \"b\" AND `c` > \"1\"",
 	}
 
-	for c, v := range cases {
-		sql := db.Debug().ToSQL(func(tx *gorm.DB) *gorm.DB {
-			parsedDB, err := Parse(c, tx.Table("table"))
-			if err != nil {
-				t.Fatal(err)
-			}
-			var lst []Table
-			return parsedDB.Find(&lst)
-		})
-		fmt.Println("sql =", sql)
-		if sql != v {
-
-			t.Fatal(c, sql, v)
+	for c, _ := range cases {
+		runner, err := NewFilterDSL(c, "table", db)
+		if err != nil {
+			t.Fatal(err)
+		}
+		parsedDB, err := runner.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+		var lst []Table
+		if err := parsedDB.Debug().Find(&lst).Error; err != nil {
+			t.Fatal(err)
 		}
 	}
 
 }
 
-func TestCase(t *testing.T) {
+func TestJoinCase(t *testing.T) {
+	dsl := "filter[a][_eq]=a&filter[bc][_eq]=1&filter[search][_contains]=10"
 	db, err := gorm.Open(sqlite.Open("test.db"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	filterQuery := "filter[a][_eq]=1"
-	var table Table
 
-	db = db.Where("store_id = 1")
-	db, err = Parse(filterQuery, db)
-	db.Last(&table)
+	db.AutoMigrate(&Table{}, &TableB{})
+	runner, err := NewFilterDSL(dsl, "table", db)
+	runner.Register("bc", func(f *Filter) func(db *gorm.DB) *gorm.DB {
+		return func(db *gorm.DB) *gorm.DB {
+			newF := f.Copy()
+			newF.TableName = "table_b"
+			newF.Field = "c"
+			db = db.Joins("LEFT JOIN table_b on table_b.a = `table`.id")
+			return newF.Run(db)
+		}
+	})
+
+	runner.Register("search", func(filter *Filter) func(db *gorm.DB) *gorm.DB {
+		return func(db *gorm.DB) *gorm.DB {
+			db = db.Where(fmt.Sprintf("`%s`.`%s` LIKE ?", filter.TableName, "b"), fmt.Sprintf("%%%s%%", filter.Value))
+			db = db.Where(fmt.Sprintf("`%s`.`%s` LIKE ?", "table_b", "b"), fmt.Sprintf("%%%s%%", filter.Value))
+			return db
+		}
+	})
+
+	parsedDB, err := runner.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var lst []Table
+	if err := parsedDB.Select("`table`.*,table_b.*").Debug().Find(&lst).Error; err != nil {
+		t.Fatal(err)
+	}
+	//select * from table left join table_b on table_b.a=`table`.id where table.a="a" and table_b.c=1 and table_b.b like '%10%' and table_b.a like '%10%'
 }
